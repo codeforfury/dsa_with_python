17a273d3b8509ac327dacdb77a401e28edb046f0
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-07 22:23:06 +0530

    DSA-Python-Learn the basics-Patterns-Problem_03

---
+++ learnthebasics/patterns/03_pattern.py
@@ -0,0 +1,15 @@
+# Problem Statement - Print 1 22 333 ... to N Right Angle Triangle pattern
+# Author - Rajiv Das
+# Date - 07-08-2026
+# ----------------------------------------------------------
+print("="*20)
+print("Pattern 03".center(10, "-"))
+print("="*20)
+
+n = int(input("Enter the no. of line you want to print the pattern: "))
+for i in range(n):
+    for j in range(i+1):
+        print(i+1, end = "")
+    print()
+
+print("="*20)
