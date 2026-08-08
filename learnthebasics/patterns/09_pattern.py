5e32466c8b6d2918fe9592edc835a6649acf692d
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-08 23:29:34 +0530

    DSA-Python-Learn the basics-Patterns-Problem_09

---
+++ learnthebasics/patterns/09_pattern.py
@@ -0,0 +1,18 @@
+# Problem Statement - Print Inverted Right aligned 12345 1234 123 to 1 Right Angle Triangle pattern
+# Author - Rajiv Das
+# Date - 08-08-2026
+# ----------------------------------------------------------
+print("="*20)
+print("Pattern 09".center(10, "-"))
+print("="*20)
+
+n = int(input("Enter the no. of line you want to print the pattern: "))
+for i in range(n,0,-1):
+    for j in range(n,i,-1):
+        print(" ", end = "")
+    
+    for j in range(1,i+1,):
+        print(j, end="")
+    print()
+
+print("="*20)
