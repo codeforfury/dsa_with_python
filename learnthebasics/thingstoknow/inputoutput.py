17707668ee2011ab1a77a271c381c37ddd60e334
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-09 00:16:05 +0530

    DSA-Python-Learn the basics-thingstoknow-inputoutput

---
+++ learnthebasics/thingstoknow/inputoutput.py
@@ -0,0 +1,7 @@
+# Problem Statement - Take an integer input from the user and prints it on the screen.
+# Author - Rajiv Das
+# Date - 09-08-2026
+# ----------------------------------------------------------
+
+n = int(input("Enter the number to print: "))
+print("The number is", n)
