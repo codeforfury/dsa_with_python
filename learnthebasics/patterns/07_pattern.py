dc4b6081cfe170ca86d73212535b971632a490cb
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-08 23:19:33 +0530

    DSA-Python-Learn the basics-Patterns-Problem_07

---
+++ learnthebasics/patterns/07_pattern.py
@@ -0,0 +1,18 @@
+# Problem Statement - Print Right aligned 1 12 123 .. to N Right Angle Triangle pattern
+# Author - Rajiv Das
+# Date - 08-08-2026
+# ----------------------------------------------------------
+print("="*20)
+print("Pattern 07".center(10, "-"))
+print("="*20)
+
+n = int(input("Enter the no. of line you want to print the pattern: "))
+for i in range(1,n+1):
+    for j in range(1,(n+1)-i):
+        print(" ", end = "")
+    
+    for j in range(i):
+        print(j+1, end="")
+    print()
+
+print("="*20)
