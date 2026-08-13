f11136008b35c089062d20baf436205310c14432
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-13 23:15:41 +0530

    DSA-Python-solve problems on array-easy-linearsearch

---
+++ solveproblemsonarrays/easy/linearsearch.py
@@ -0,0 +1,19 @@
+# Linear Search.
+
+# Problem Statement - Given an array, and an element num the task is to 
+# find if num is present in the given array or not. If present print the 
+# index of the element or print -1.
+
+# Author - Rajiv Das
+# Date - 13-08-2026
+# ----------------------------------------------------------
+
+arr = [5,8,2,3,7,1,20,10]
+num = 20
+for i in range(len(arr)):
+    if arr[i] == num:
+        print(i)
+        break
+
+else:
+    print(-1)
