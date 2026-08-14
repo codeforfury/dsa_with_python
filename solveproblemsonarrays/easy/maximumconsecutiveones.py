a563b37ff2be1a91a93c4d02754b8a4bb13c6115
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-15 00:02:34 +0530

    DSA-Python-solve problems on array-easy-maxconsecutiveones

---
+++ solveproblemsonarrays/easy/maximumconsecutiveones.py
@@ -0,0 +1,22 @@
+# Count Maximum Consecutive One's in the array
+
+# Problem Statement - Given an array that contains only 1 and 0 return the count 
+# of maximum consecutive ones in the array.
+# Time complexity - O(n)
+# Space Complexity - O(1)
+
+# Author - Rajiv Das
+# Date - 14-08-2026
+# ----------------------------------------------------------
+
+arr = [1,1,1,0,0,1,1,0,1]
+c = 0
+maximum = 0
+for i in arr:
+    if i == 1:
+        c += 1
+        maximum = max(maximum, c)
+    else:
+        c = 0
+
+print("Max no. of one's:", maximum)
