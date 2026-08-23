b75d50b0cf0ebafde449b12279de626f0a368cb4
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-23 23:03:05 +0530

    DSA-Python-learnthebasics-recursion-sumnatural

--- learnthebasics/recursion/sumnatural.py
+++ learnthebasics/recursion/sumnatural.py
@@ -6,9 +6,17 @@
 # ----------------------------------------------------------
 
 # Three ways to do this - 
-# 1) Brute Force approach i.e. using loop from 1 to N [takes O(n) time complexity]
-# 2) Recursion approach which we will do now [takes O(n) time complexity]
-# 3) Optimal approach i.e. using formula - n(n+1)/2 [takes O(1) time complexity] will do this also. 
+# 1) Brute Force approach i.e. using loop from 1 to N.
+# Time complexity - O(n)
+# Space complexity - O(1)
+
+# 2) Recursion approach which we will do now.
+# Time complexity - O(n)
+# Space complexity - O(n)
+
+# 3) Optimal approach i.e. using formula - n(n+1)/2 will do this also.
+# Time complexity - O(1)
+# Space complexity - O(1) 
 
 
 # 2) Recursion approach - 
