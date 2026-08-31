945c0da82ba8184b80c39abfa357f60d8162e31a
Rajiv Das <dasrajiv6507@gmail.com>
2026-08-31 23:58:00 +0530

    Improve while loops program

--- learnthebasics/thingstoknow/whileloops.py
+++ learnthebasics/thingstoknow/whileloops.py
@@ -1,3 +1,5 @@
+# While Loops.
+
 # Problem Statement - Given a digit d (0 to 9), find the sum of the first 50 
 # positive integers (integers > 0) that end with digit d. 
 # A number ends with digit d if its last digit is d.
